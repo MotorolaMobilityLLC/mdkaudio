ec85361de6663b39fcfd82468c9adcbc9cf39ec0
a22460 <a22460@motorola.com>
2016-09-27 13:47:27 -0500

    IKMODS-1180 [MDK Apps] Show Dip Switch Config
    Below the status, add a collapsible card that shows
    the dip switch configuration.
    
    Change-Id: I77a0599b41f74427ead4d7ece23048457eefff0a
    Reviewed-on: https://gerrit.mot.com/903338
    SME-Granted: SME Approvals Granted
    Reviewed-by: Localization Verification <l10nstr@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christian Flowers <flowers@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- app/src/main/java/com/motorola/samples/mdkaudio/MainActivity.java
+++ app/src/main/java/com/motorola/samples/mdkaudio/MainActivity.java
@@ -28,6 +28,7 @@
 
 package com.motorola.samples.mdkaudio;
 
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
@@ -41,6 +42,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
@@ -79,6 +82,26 @@ public class MainActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setActionBar(toolbar);
+
+        LinearLayout dipTitle = (LinearLayout)findViewById(R.id.layout_dip_description_title);
+        dipTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout dipDescription = (LinearLayout)findViewById(R.id.layout_dip_description);
+                ImageView imgExpand = (ImageView)findViewById(R.id.imageview_description_img);
+
+                if (dipDescription.getVisibility() == View.GONE) {
+                    dipDescription.setVisibility(View.VISIBLE);
+                    imgExpand.setImageResource(R.drawable.ic_expand_less);
+                } else {
+                    dipDescription.setVisibility(View.GONE);
+                    imgExpand.setImageResource(R.drawable.ic_expand_more);
+                }
+
+                dipDescription.setPivotY(0);
+                ObjectAnimator.ofFloat(dipDescription, "scaleY", 0f, 1f).setDuration(300).start();
+            }
+        });
 
         TextView textView = (TextView)findViewById(R.id.mod_external_dev_portal);
         if (textView != null) {
